fca88d8866c0566620c609a16c52efff3a3a4dfc
Unc Sam <aisaasbusiness@gmail.com>
2025-08-24 01:40:52 +0000

    there' still bugs. see screenshot. for example, when the selected right

--- src/components/painscribe/BodyDiagram.tsx
+++ src/components/painscribe/BodyDiagram.tsx
@@ -19,33 +19,34 @@ interface BodyDiagramProps {
 }
 
 const bodyPartHotspots: { id: BodyPart; x: number; y: number; width: number; height: number; view: 'front' | 'back' }[] = [
-  // Front
-  { id: 'head', x: 40, y: 2, width: 20, height: 13, view: 'front' },
-  { id: 'chest', x: 33, y: 16, width: 34, height: 12, view: 'front' },
-  { id: 'abdomen', x: 35, y: 28, width: 30, height: 10, view: 'front' },
-  { id: 'pelvis', x: 35, y: 38, width: 30, height: 8, view: 'front' },
-  { id: 'left-shoulder', x: 25, y: 16, width: 10, height: 5, view: 'front' },
-  { id: 'right-shoulder', x: 65, y: 16, width: 10, height: 5, view: 'front' },
-  { id: 'left-arm', x: 18, y: 22, width: 15, height: 35, view: 'front' },
-  { id: 'right-arm', x: 67, y: 22, width: 15, height: 35, view: 'front' },
-  { id: 'left-hand', x: 15, y: 57, width: 10, height: 10, view: 'front' },
-  { id: 'right-hand', x: 75, y: 57, width: 10, height: 10, view: 'front' },
-  { id: 'left-leg', x: 35, y: 46, width: 15, height: 40, view: 'front' },
-  { id: 'right-leg', x: 50, y: 46, width: 15, height: 40, view: 'front' },
-  { id: 'left-foot', x: 35, y: 86, width: 15, height: 8, view: 'front' },
-  { id: 'right-foot', x: 50, y: 86, width: 15, height: 8, view: 'front' },
-  // Back
-  { id: 'head', x: 40, y: 2, width: 20, height: 13, view: 'back' },
-  { id: 'upper-back', x: 33, y: 16, width: 34, height: 12, view: 'back' },
-  { id: 'lower-back', x: 35, y: 28, width: 30, height: 10, view: 'back' },
-  { id: 'left-arm', x: 18, y: 22, width: 15, height: 35, view: 'back' },
-  { id: 'right-arm', x: 67, y: 22, width: 15, height: 35, view: 'back' },
-  { id: 'left-hand', x: 15, y: 57, width: 10, height: 10, view: 'back' },
-  { id: 'right-hand', x: 75, y: 57, width: 10, height: 10, view: 'back' },
-  { id: 'left-leg', x: 35, y: 46, width: 15, height: 40, view: 'back' },
-  { id: 'right-leg', x: 50, y: 46, width: 15, height: 40, view: 'back' },
-  { id: 'left-foot', x: 35, y: 86, width: 15, height: 8, view: 'back' },
-  { id: 'right-foot', x: 50, y: 86, width: 15, height: 8, view: 'back' },
+    // Front view (viewer's perspective)
+    { id: 'head', x: 40, y: 2, width: 20, height: 13, view: 'front' },
+    { id: 'chest', x: 33, y: 16, width: 34, height: 12, view: 'front' },
+    { id: 'abdomen', x: 35, y: 28, width: 30, height: 10, view: 'front' },
+    { id: 'pelvis', x: 35, y: 38, width: 30, height: 8, view: 'front' },
+    { id: 'right-shoulder', x: 25, y: 16, width: 10, height: 5, view: 'front' }, // Patient's Right
+    { id: 'left-shoulder', x: 65, y: 16, width: 10, height: 5, view: 'front' }, // Patient's Left
+    { id: 'right-arm', x: 18, y: 22, width: 15, height: 35, view: 'front' }, // Patient's Right
+    { id: 'left-arm', x: 67, y: 22, width: 15, height: 35, view: 'front' }, // Patient's Left
+    { id: 'right-hand', x: 15, y: 57, width: 10, height: 10, view: 'front' }, // Patient's Right
+    { id: 'left-hand', x: 75, y: 57, width: 10, height: 10, view: 'front' }, // Patient's Left
+    { id: 'right-leg', x: 35, y: 46, width: 15, height: 40, view: 'front' }, // Patient's Right
+    { id: 'left-leg', x: 50, y: 46, width: 15, height: 40, view: 'front' }, // Patient's Left
+    { id: 'right-foot', x: 35, y: 86, width: 15, height: 8, view: 'front' }, // Patient's Right
+    { id: 'left-foot', x: 50, y: 86, width: 15, height: 8, view: 'front' }, // Patient's Left
+    
+    // Back view (viewer's perspective)
+    { id: 'head', x: 40, y: 2, width: 20, height: 13, view: 'back' },
+    { id: 'upper-back', x: 33, y: 16, width: 34, height: 12, view: 'back' },
+    { id: 'lower-back', x: 35, y: 28, width: 30, height: 10, view: 'back' },
+    { id: 'left-arm', x: 18, y: 22, width: 15, height: 35, view: 'back' },
+    { id: 'right-arm', x: 67, y: 22, width: 15, height: 35, view: 'back' },
+    { id: 'left-hand', x: 15, y: 57, width: 10, height: 10, view: 'back' },
+    { id: 'right-hand', x: 75, y: 57, width: 10, height: 10, view: 'back' },
+    { id: 'left-leg', x: 35, y: 46, width: 15, height: 40, view: 'back' },
+    { id: 'right-leg', x: 50, y: 46, width: 15, height: 40, view: 'back' },
+    { id: 'left-foot', x: 35, y: 86, width: 15, height: 8, view: 'back' },
+    { id: 'right-foot', x: 50, y: 86, width: 15, height: 8, view: 'back' },
 ];
 
 

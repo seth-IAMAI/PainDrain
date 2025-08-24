0769316e315a0a0d7a057c47003528707b9930f0
Unc Sam <aisaasbusiness@gmail.com>
2025-08-24 11:59:56 +0000

    cleanup

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cZ6Zu99/d6tR7A1MMkvBSlEE10g74ltNzVSEN0hlQUg=\"\n}"
